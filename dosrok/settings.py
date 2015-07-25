61d4d16f7f8e70bef5144500b908e0ae8c96cf0d
alexsyrom <alexsyrom@yandex.ru>
2015-07-25 15:20:41 +0300

    database settings + local_settings

--- dosrok/settings.py
+++ dosrok/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'dosrok.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dosrok',
+        'USER': 'root',
+        'PASSWORD': 'gsogso;;;',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -85,9 +89,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -100,3 +104,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+from dosrok.local_settings import *
