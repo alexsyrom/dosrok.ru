37a9a201eadbeb429c450750750635226d52a10a
alexsyrom <alexsyrom@yandex.ru>
2015-07-27 13:56:37 +0300

    desirable view

--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Problem, Student
 
-# Create your views here.
+class Index(generic.ListView):
+   model = Problem
+   template_name = 'main/index.html'
+   context_object_name = 'problem_list'
+   def get_context_data(self, *args, **kwargs):
+       context = super(Index, self).get_context_data(*args, **kwargs)
+       context['student_list'] = Student.objects.all()
+       return context
