367f97cbab4dea5d8eba2ecca4bf06165f1ebd2e
alexsyrom <alexsyrom@yandex.ru>
2015-08-09 18:00:17 +0300

    ternary fail

--- dosrok/urls.py
+++ dosrok/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     url(r'^$', Index.as_view(), name='index'),
     url(r'^admin_tools/', include('admin_tools.urls')),
     url(r'^admin/', include(admin.site.urls)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) if settings.DEBUG else []
+] + (static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) if settings.DEBUG else [])
