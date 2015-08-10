a7632dfbd53e4bc3dace2fcd2f6a4284806829eb
alexsyrom <alexsyrom@yandex.ru>
2015-08-10 15:55:32 +0300

    clear admin page

--- dosrok/settings.py
+++ dosrok/settings.py
@@ -77,6 +77,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'dosrok.wsgi.application'
 
+ADMIN_TOOLS_INDEX_DASHBOARD = 'dosrok.dashboard.CustomIndexDashboard'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
